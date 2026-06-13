9799f65aee1352b9b57bf42a25ae6c15aa955994
王水群 <16331161+wang-shuiqun@user.noreply.gitee.com>
2026-06-13 11:19:19 +0800

    创建项目

---
+++ to-my-l/src/main/java/com/panpeixue/myl/ToMyLApplication.java
@@ -0,0 +1,13 @@
+package com.panpeixue.myl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToMyLApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ToMyLApplication.class, args);
+  }
+
+}
---
+++ to-my-l/src/test/java/com/panpeixue/myl/ToMyLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panpeixue.myl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToMyLApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
